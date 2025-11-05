4939835d28adb1ec3bb06adbc75287dae6d7a3dc
Felipe6677 <felipeprates@colegiopaulodetarso.com.br>
2025-11-05 15:32:49 +0000

    salvo

--- app.py
+++ app.py
@@ -13,3 +13,12 @@ def info():
    modulo = "flask"
    aula = "1"
    return f"<h1>modulo: {modulo}, aula: {aula} </h1>"
+
+app.route('/bemvindo/<usuario>')
+def bemvindo(usuario):
+   modulo = "css"
+   aula = "1"
+   return f"<h1>modulo: {usuario.capitalize()}, aula: {aula} </h1>"
+
+
+
